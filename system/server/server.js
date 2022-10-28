8cfa756ca88563d8c81c347ce38a3d3a3f308c4c
Antonello Ceravola <55833052+antonelloceravola@users.noreply.github.com>
2022-10-28 20:53:49 +0200

    Add stat info

--- system/server/server.js
+++ system/server/server.js
@@ -126,6 +126,31 @@ class ExecuteScript {
   }
 }
 
+/*
+
+  fs.stat('README.md',(e,s)=> console.log(s))
+> Stats {
+    dev: 16777234,
+    mode: 33188,
+    nlink: 1,
+    uid: 501,
+    gid: 20,
+    rdev: 0,
+    blksize: 4096,
+    ino: 15428310,
+    size: 46,
+    blocks: 8,
+    atimeMs: 1647331446275.954,
+    mtimeMs: 1636134156213.5515,
+    ctimeMs: 1636134156213.5515,
+    birthtimeMs: 1636134156213.3267,
+    atime: 2022-03-15T08:04:06.276Z,
+    mtime: 2021-11-05T17:42:36.214Z,
+    ctime: 2021-11-05T17:42:36.214Z,
+    birthtime: 2021-11-05T17:42:36.213Z
+  }
+*/
+
 // Create Client Configuration
 const fs = require( 'fs' );
 const userName = config.client.host.name;
