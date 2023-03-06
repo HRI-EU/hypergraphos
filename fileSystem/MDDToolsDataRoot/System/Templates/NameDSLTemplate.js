6cad6881813ea4fad9c4c2d0cf4300774164f9bf
Antonello Ceravola <55833052+antonelloceravola@users.noreply.github.com>
2023-03-06 20:26:41 +0100

    Update DSL templates

--- fileSystem/MDDToolsDataRoot/System/Templates/NameDSLTemplate.js
+++ fileSystem/MDDToolsDataRoot/System/Templates/NameDSLTemplate.js
@@ -10,39 +10,76 @@ Date: 10.07.2020
 =============================================================================
 */
 
-function DefaultDSL_includeList() {
+//--------------------------------------------------
+// Define list of include files (Optional Function)
+//--------------------------------------------------
+function NameDSL_includeList() {
+  //
+  // Example:
+  //   return(['/fileServer/System/Includes/DSLInclude.js']);
+  //
   return([]);
 }
-function DefaultDSL_getDSL( g ) {
+
+//--------------------------------------------------
+// Define setup of DSL
+//--------------------------------------------------
+function KanbanDSL_setupDSL( g ) {
+}
+
+//--------------------------------------------------
+// Define DSL Meta-Model and DSL Model
+//--------------------------------------------------
+function NameDSL_getDSL( g ) {
+
+  //----------------------------------
+  // Define DSL Meta-Models functions
+  //----------------------------------
+  const Name_Node1 = ()=> {
+    return( $(go.Node, "Auto",              // the Shape automatically fits around the TextBlock
+      $(go.Shape, "RoundedRectangle",       // use this kind of figure for the Shape
+        new go.Binding("fill", "color")),   // bind Shape.fill to Node.data.color
+      $(go.TextBlock,
+        { margin: 3 },                      // some room around the text
+        new go.Binding("text", "label"))    // bind TextBlock.text to Node.data.label
+    ));
+  };
+
+  //----------------------------------
+  // Define DSL data
+  //----------------------------------
   const dsl = {
-    // Define DSL templates
+    
+    //-----------------------------------
+    // Define DSL Meta-Models categories
+    //-----------------------------------
     templateNodeList: [
-      { category: 'Default', template: ()=>{
-        return( $(go.Node, "Auto",  // the Shape automatically fits around the TextBlock
-          $(go.Shape, "RoundedRectangle",  // use this kind of figure for the Shape
-            // bind Shape.fill to Node.data.color
-            new go.Binding("fill", "color")),
-          $(go.TextBlock,
-            { margin: 3 },  // some room around the text
-            // bind TextBlock.text to Node.data.key
-            new go.Binding("text", "key"))
-        ));
-      }},
+      { category: 'Name_Node1', template: Name_Node1 },
     ],
+
+    //-----------------------------------
+    // Define DSL Models Node Elements
+    //-----------------------------------
     dataNodeList:[
       {
-        'key': 'key0',
+        'label': 'Red Node',
         'color': 'Red',
-        'category': 'Default',
+        'category': 'Name_Node1',
       },
       {
-        'key': 'key1',
+        'label': 'Green Node',
         'color': 'Green',
-        'category': 'Default',
+        'category': 'Name_Node1',
       },
     ],
+
+    //-----------------------------------
+    // Define DSL Models Link Elements
+    //-----------------------------------
     templateLinkList:[],
     dataLinkList:[],
   };
+
+  // Return DSL data
   return( dsl );
 }
