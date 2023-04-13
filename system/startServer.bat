7614da32f9d3460dfcaef57c082387464eb2ff1f
Antonello Ceravola <55833052+antonelloceravola@users.noreply.github.com>
2023-04-13 18:08:10 +0200

    Update on Copyright

--- system/startServer.bat
+++ system/startServer.bat
@@ -1,9 +1,9 @@
 REM
 REM =============================================================================
-REM Licensed Materials - Property of LorinWare Prod.
-REM (C) Copyright Christophe LORIN 2021, All Rights Reserved.
+REM Licensed Materials - Property of Frank Joublin and Antonio Ceravola.
+REM (C) Copyright Frank Joublin and Antonio Ceravola. 2021, All Rights Reserved.
 REM France Government Users Restricted Rights - Use, duplication or disclosure
-REM restricted by GSA ADP Schedule Contract with Christophe LORIN.
+REM restricted by GSA ADP Schedule Contract with Frank Joublin and Antonio Ceravola.
 REM =============================================================================
 REM Module: MDDTools Server Startup File
 REM Date: 10.07.2020
