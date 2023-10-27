835a4e8e40c0a162ec5e5cccee6373466786b853
Cristian Sandu <cristian.gh.sandu@gmail.com>
2023-10-27 15:22:58 +0300

    Add Cristian configs

--- system/serverConfig.js
+++ system/serverConfig.js
@@ -128,7 +128,7 @@ if( ( config.host.hostname == 'Antonellos-Mini' ) ||
   // List of remote host
   config.client.remoteHost['NUC'] = '192.168.1.11:7575';
 
-} else if( ( config.host.hostname == 'Main-PC' ) && 
+} else if( ( config.host.hostname == 'Main-PC' ) &&
            ( config.host.username == 'Frank Joublin' ) ) {
            // C:\Users\frank\Dropbox\DevAll\MDDToolsDataRoot
   config.server.dataRoot = '../../../../../Dropbox/DevAll/MDDToolsDataRoot';
@@ -140,7 +140,7 @@ if( ( config.host.hostname == 'Antonellos-Mini' ) ||
   config.client.graph.isDoubleClickCreateNodeEnabled = false;
   // List of remote host
   config.client.remoteHost['NUC'] = '192.168.178.25:7575';
-} else if( ( config.host.hostname == 'QuadCore' ) && 
+} else if( ( config.host.hostname == 'QuadCore' ) &&
             ( config.host.username == 'Christophe' ) ) {
   config.client.host.name = 'Christophe';
   config.client.host.statusURL = '/fileServer/Users/Christophe_status.json';
@@ -197,6 +197,16 @@ if( ( config.host.hostname == 'Antonellos-Mini' ) ||
   config.server.tempRoot = '../../../../../DevAll//MDDToolsTempRoot';
   config.client.host.name = 'Frank';
   config.client.host.statusURL = '/fileServer/Users/Frank_status.json';
+  config.client.graph.allowDeleteKey = true;
+  config.client.graph.isDoubleClickCreateNodeEnabled = false;
+} else if( config.host.username == 'cristian' )
+{
+  config.server.dataRoot = '../../../../../Dropbox/DevAll/MDDToolsDataRoot';
+  config.server.deployRoot = '../../../../../Dropbox/DevAll/MDDToolsDeployRoot';
+  config.server.tempRoot = '../../../../../Dropbox/DevAll/MDDToolsTempRoot';
+  config.server.webServerPort = 8080;
+  config.client.host.name = 'Cristian';
+  config.client.host.statusURL = '/fileServer/Users/Cristian_status.json';
   config.client.graph.allowDeleteKey = true;
   config.client.graph.isDoubleClickCreateNodeEnabled = false;
 }
