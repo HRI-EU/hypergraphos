d6410d0f982c049ddb0c8fef14ece51c72f90530
Johane Takeuchi <johane.takeuchi@jp.honda-ri.com>
2024-02-01 12:54:05 +0900

    change johane's account

--- system/serverConfig.js
+++ system/serverConfig.js
@@ -185,7 +185,7 @@ if( ( config.host.hostname == 'Antonellos-Mini' ) ||
   config.client.host.statusURL = '/fileServer/Users/Antonello_status.json';
   config.client.graph.allowDeleteKey = false;
   config.client.graph.isDoubleClickCreateNodeEnabled = false;
-} else if( config.host.username == "johane" ) {
+} else if( config.host.username == "982979" ) {
   config.server.dataRoot = '../../../../../Dropbox/DevAll/MDDToolsDataRoot';
   config.server.deployRoot = '../../../../../Dropbox/DevAll/MDDToolsDeployRoot';
   config.server.tempRoot = '../../../../../Dropbox/DevAll/MDDToolsTempRoot';
