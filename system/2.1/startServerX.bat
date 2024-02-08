509ea2f2c5826e86f010af618b79edc614274b18
Antonello Ceravola <55833052+antonelloceravola@users.noreply.github.com>
2024-02-08 11:00:33 +0100

    Create startServerX.bat

---
+++ system/2.1/startServerX.bat
@@ -0,0 +1,18 @@
+@echo off
+REM
+REM =============================================================================
+REM Licensed Materials - Property of Frank Joublin and Antonio Ceravola.
+REM (C) Copyright Frank Joublin and Antonio Ceravola. 2021, All Rights Reserved.
+REM France Government Users Restricted Rights - Use, duplication or disclosure
+REM restricted by GSA ADP Schedule Contract with Frank Joublin and Antonio Ceravola.
+REM =============================================================================
+REM Module: MDDTools Server Startup File
+REM Date: 10.07.2020
+REM =============================================================================
+REM
+
+cd server
+IF NOT exist ..\node_modules\ npm install
+
+node server-express.js %*
+cd ..
