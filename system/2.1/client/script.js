2ff5ae6d4efd063272c3f2b3c6277f4a2f046773
Antonello Ceravola <55833052+antonelloceravola@users.noreply.github.com>
2024-02-08 13:57:33 +0100

    Indentations

--- system/2.1/client/script.js
+++ system/2.1/client/script.js
@@ -1,129 +1,120 @@
-var formLogin = document.getElementById("login-form");
-var formRegister = document.getElementById("register-form");
+/*
+=============================================================================
+Licensed Materials - Property of Frank Joublin and Antonio Ceravola.
+(C) Copyright Frank Joublin and Antonio Ceravola. 2021, All Rights Reserved.
+France Government Users Restricted Rights - Use, duplication or disclosure
+restricted by GSA ADP Schedule Contract with Frank Joublin and Antonio Ceravola.
+=============================================================================
+Module: Client Scripts for Login
+Date: 10.07.2020
+=============================================================================
+*/
 
-var formEmail = document.getElementById("email");
-var formPassword = document.getElementById("password");
+var formLogin = document.getElementById( 'login-form' );
+var formRegister = document.getElementById( 'register-form' );
 
-var formNameRegister = document.getElementById("register-name");
-var formEmailRegister = document.getElementById("register-email");
-var formPasswordRegister = document.getElementById("register-password");
+var formEmail = document.getElementById( 'email' );
+var formPassword = document.getElementById( 'password' );
 
-var tabRegister = document.getElementById("tabRegister");
-var tabLogin = document.getElementById("tabLogin");
+var formNameRegister = document.getElementById( 'register-name' );
+var formEmailRegister = document.getElementById( 'register-email' );
+var formPasswordRegister = document.getElementById( 'register-password' );
 
+var tabRegister = document.getElementById( 'tabRegister' );
+var tabLogin = document.getElementById( 'tabLogin' );
 
-
-tabRegister.addEventListener("click", function(e) {
+tabRegister.addEventListener( 'click', function(e) {
   e.preventDefault();
-
-  hide(formLogin);
-  show(formRegister);
+  hide( formLogin );
+  show( formRegister );
 });
 
-tabLogin.addEventListener("click", function(e) {
+tabLogin.addEventListener( 'click', function(e) {
   e.preventDefault();
-    show(formLogin);
-    hide(formRegister);
+  show( formLogin );
+  hide( formRegister );
 });
 
-formLogin.addEventListener("submit", async(e)=> doLogin(e));
+formLogin.addEventListener( 'submit', async(e)=> doLogin( e ) );
+formRegister.addEventListener( 'submit', async(e)=> doRegister( e ) );
 
-formRegister.addEventListener("submit", async(e)=> doRegister(e));
+async function doLogin( e ) {
+  e.preventDefault();
+  const email = formEmail.value;
+  const password = formPassword.value;
 
+  let response = await sendLoginPayload({ email: email, password: password });
+  let jsonResponse = await response.json();
 
-async function doLogin(e){
-    e.preventDefault();
-      const email = formEmail.value;
-      const password = formPassword.value;
-  
-      let response = await sendLoginPayload({email: email, password: password});
-      let jsonResponse = await response.json();
-  
-      if(response.status == 200){
-        //localStorage.setItem('token', jsonResponse.token);
-        window.location.replace("/indexHG.html")
-      }else{
-          let errorsContainer = document.getElementsByClassName('errors')[0];
-          errorText.innerHTML = jsonResponse.error;
-  
-          errorsContainer.classList.remove('form--hidden');
-          setTimeout(() => {
-            errorsContainer.classList.add('form--hidden');
-  
-          }, 2000);
-      }
-  
+  if( response.status == 200 ){
+    //localStorage.setItem('token', jsonResponse.token);
+    window.location.replace( '/indexHG.html' );
+  } else {
+    let errorsContainer = document.getElementsByClassName( 'errors' )[0];
+    errorText.innerHTML = jsonResponse.error;
+
+    errorsContainer.classList.remove( 'form--hidden' );
+    setTimeout( ()=> errorsContainer.classList.add( 'form--hidden' ), 2*1000 );
+  }
+}
+async function doRegister( e ) {
+  e.preventDefault();
+  const name = formNameRegister.value;
+  const email = formEmailRegister.value;
+  const password = formPasswordRegister.value;
+
+  let response = await sendRegisterPayload({ email: email, password: password, name: name });
+  let jsonResponse = await response.json();
+
+  if( ( response.status == 200 ) || ( response.status == 201 ) ) {
+    show( formLogin );
+    hide( formRegister );
+  } else {
+    let errorsContainer = document.getElementsByClassName( 'errors' )[0];
+    errorText.innerHTML = jsonResponse.error;
+
+    errorsContainer.classList.remove( 'form--hidden' );
+    setTimeout(()=> errorsContainer.classList.add( 'form--hidden' ), 2*1000 );
   }
 
-  async function doRegister(e){
-    e.preventDefault();
+}
+async function sendLoginPayload( data ) {
+  try {
+    const response = await fetch( '/auth/login', {
+      method: 'POST', // or 'PUT'
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify( data ),
+    });
 
-    const name = formNameRegister.value;
-    const email = formEmailRegister.value;
-    const password = formPasswordRegister.value;
-
-    let response = await sendRegisterPayload({email: email, password: password, name: name});
-    let jsonResponse = await response.json();
-  
-      if(response.status == 200 || response.status == 201){
-        show(formLogin);
-        hide(formRegister);
-      }else{
-          let errorsContainer = document.getElementsByClassName('errors')[0];
-          errorText.innerHTML = jsonResponse.error;
-  
-          errorsContainer.classList.remove('form--hidden');
-          setTimeout(() => {
-            errorsContainer.classList.add('form--hidden');
-  
-          }, 2000);
-      }
-
+    return( response );
+    //return result;
+  } catch( error ) {
+    console.error( 'Error:', error );
   }
+}
+async function sendRegisterPayload( data ) {
+  try {
+    const response = await fetch( '/auth/register', {
+      method: 'POST', // or 'PUT'
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify( data ),
+    });
 
-
-  async function sendLoginPayload(data){
-    try {
-      const response = await fetch("/auth/login", {
-        method: "POST", // or 'PUT'
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(data),
-      });
-  
-      return response;
-    
-      //return result;
-    } catch (error) {
-      console.error("Error:", error);
-    }
+    return response;
+    //return result;
+  } catch( error ) {
+    console.error( 'Error:', error );
   }
-
-  async function sendRegisterPayload(data){
-    try {
-      const response = await fetch("/auth/register", {
-        method: "POST", // or 'PUT'
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(data),
-      });
-  
-      return response;
-    
-      //return result;
-    } catch (error) {
-      console.error("Error:", error);
-    }
-  }
-
-  function hide(elem) {
-    elem.classList.add("form--hidden");
-    elem.classList.remove("form--unhidden");
-  }
-  
-  function show(elem) {
-  elem.classList.add("form--unhidden");
-  elem.classList.remove("form--hidden");
-  }
+}
+function hide( elem ) {
+  elem.classList.add( 'form--hidden' );
+  elem.classList.remove( 'form--unhidden' );
+}  
+function show( elem ) {
+  elem.classList.add( 'form--unhidden' );
+  elem.classList.remove( 'form--hidden' );
+}
