ea3d33e8d5a6de33373cdc68de3c1466fd958ca5
Antonello Ceravola <55833052+antonelloceravola@users.noreply.github.com>
2024-04-19 11:29:49 +0200

    First import

---
+++ system/2.1/server/sconfigs/Ebbie_sconfig.js
@@ -0,0 +1,11 @@
+function userSetup( config ) {
+  if( config.host.username == "RE900575" ) {
+    config.server.dataRoot = '../../../../../../Dropbox/DevAll/MDDToolsDataRoot';
+    config.server.deployRoot = '../../../../../../Dropbox/DevAll/MDDToolsDeployRoot';
+    config.server.tempRoot = '../../../../../../Dropbox/DevAll/MDDToolsTempRoot';
+    config.client.host.name = 'Ebbie';
+    config.client.host.statusURL = '/fileServer/Users/Ebbie_status.json';
+    config.client.graph.allowDeleteKey = false;
+  }
+}
+module.exports = userSetup;
