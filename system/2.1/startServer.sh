00fc5150ed9f9fc70e420dfb11db3fec36e45e00
Frank Joublin <frank.joublin@honda-ri.de>
2024-10-24 09:14:54 +0000

    Update startServer.sh header

--- system/2.1/startServer.sh
+++ system/2.1/startServer.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
+# Copyright (c) 2024 Antonello Ceravola, Frank Joublin, Honda Research Institute Europe GmbH
+# 
+# This file is part of HyperGraphOS.
+# 
+# This source code is licensed under the MIT License found in the
+# LICENSE file in the root directory of this source tree.
+
 #
 # <StartServer>
-#
-# Copyright (C)
-# Honda Research Institute Europe GmbH
-# Carl-Legien-Str. 30
-# 63073 Offenbach/Main
-# Germany
-#
-# UNPUBLISHED PROPRIETARY MATERIAL.
-# ALL RIGHTS RESERVED.
-#
 #
 
 set -euo pipefail
