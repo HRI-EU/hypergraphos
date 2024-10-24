2b9ccca17c9bccd9958ae2f8d46e63cc51566dfc
Frank Joublin <frank.joublin@honda-ri.de>
2024-10-24 09:39:31 +0000

    Update ExploreEditorWrapper.js

--- system/2.1/client/editors/ExploreEditorWrapper.js
+++ system/2.1/client/editors/ExploreEditorWrapper.js
@@ -1,11 +1,6 @@
 /*
 =============================================================================
-Licensed Materials - Property of Frank Joublin and Antonio Ceravola.
-(C) Copyright Frank Joublin and Antonio Ceravola. 2021, All Rights Reserved.
-France Government Users Restricted Rights - Use, duplication or disclosure
-restricted by GSA ADP Schedule Contract with Frank Joublin and Antonio Ceravola.
-=============================================================================
-Module: MDDTools EditorJS Interface
+Module: HyperGraphOS EditorJS Interface
 Date: 10.10.2020
 =============================================================================
 */
