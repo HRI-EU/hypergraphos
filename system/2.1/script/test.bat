cc42696486e121c80f3c095990a00147c5cdc8a3
Frank Joublin <frank.joublin@honda-ri.de>
2024-10-24 11:38:57 +0000

    Update test.bat

--- system/2.1/script/test.bat
+++ system/2.1/script/test.bat
@@ -1,2 +1,10 @@
+REM Copyright (c) 2024 Antonello Ceravola, Frank Joublin, Honda Research Institute Europe GmbH
+REM 
+REM This file is part of HyperGraphOS.
+REM 
+REM This source code is licensed under the MIT License found in the
+REM LICENSE file in the root directory of this source tree.
+REM 
+
 @echo "test.sh executed" > ./output.txt
 @echo "test.sh has been executed" %*
