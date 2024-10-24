ee453c1bc8ca574460679507a01d6543b0d631f0
Frank Joublin <frank.joublin@honda-ri.de>
2024-10-24 11:34:30 +0000

    Update copyAvatarSources1.sh

--- system/2.1/script/copyAvatarSources1.sh
+++ system/2.1/script/copyAvatarSources1.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
-#
-# <avatarsource>
-#
-# Copyright (C)
-# Honda Research Institute Europe GmbH
-# Carl-Legien-Str. 30
-# 63073 Offenbach/Main
-# Germany
-#
-# UNPUBLISHED PROPRIETARY MATERIAL.
-# ALL RIGHTS RESERVED.
-#
-#
+# Copyright (c) 2024 Antonello Ceravola, Frank Joublin, Honda Research Institute Europe GmbH
+# 
+# This file is part of HyperGraphOS.
+# 
+# This source code is licensed under the MIT License found in the
+# LICENSE file in the root directory of this source tree.
 
 set -euo pipefail
 
