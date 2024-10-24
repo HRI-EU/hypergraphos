c7b20b5b2c47b68e678c816f55b672797839a122
Frank Joublin <frank.joublin@honda-ri.de>
2024-10-24 08:59:38 +0000

    Update simpleFileServerConfig.js

--- system/2.1/simpleFileServer/simpleFileServerConfig.js
+++ system/2.1/simpleFileServer/simpleFileServerConfig.js
@@ -1,11 +1,3 @@
-/*
-Copyright (c) 2024 Antonello Ceravola, Frank Joublin, Honda Research Institute Europe GmbH
-
-This file is part of HyperGraphOS.
-
-This source code is licensed under the MIT License found in the
-LICENSE file in the root directory of this source tree.
-*/
 
 /*
 =============================================================================
