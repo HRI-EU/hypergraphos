ba73168b245b5a81e4fbe57a9eabaa31649d1b7d
Frank Joublin <frank.joublin@honda-ri.de>
2024-10-24 09:39:12 +0000

    Update Editors.js

--- system/2.1/client/editors/Editors.js
+++ system/2.1/client/editors/Editors.js
@@ -1,11 +1,6 @@
 /*
 =============================================================================
-Licensed Materials - Property of Frank Joublin and Antonio Ceravola.
-(C) Copyright Frank Joublin and Antonio Ceravola. 2021, All Rights Reserved.
-France Government Users Restricted Rights - Use, duplication or disclosure
-restricted by GSA ADP Schedule Contract with Frank Joublin and Antonio Ceravola.
-=============================================================================
-Module: MDDTools Editors and Viewers
+Module: HyperGraphOS Editors and Viewers
 Date: 10.07.2020
 =============================================================================
 */
