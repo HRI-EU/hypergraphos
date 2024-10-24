8316dde18b1969f76e2f7a3e6e3e77f0de5b5100
Frank Joublin <frank.joublin@honda-ri.de>
2024-10-24 11:31:43 +0000

    Update copyAvatarSources.bat

--- system/2.1/script/copyAvatarSources.bat
+++ system/2.1/script/copyAvatarSources.bat
@@ -1,3 +1,11 @@
+REM Copyright (c) 2024 Antonello Ceravola, Frank Joublin, Honda Research Institute Europe GmbH
+REM 
+REM This file is part of HyperGraphOS.
+REM 
+REM This source code is licensed under the MIT License found in the
+REM LICENSE file in the root directory of this source tree.
+REM 
+
 node C:\Users\frank\Documents\MDDToolsHome\MDDTools\system\server\server2 remoteHost:192.168.178.25 remotePort:7575 url:/getRules_Receptionist.js sourceFile:C:/Users/frank/Dropbox/DevAll/MDDToolsDataRoot/00/31.js
 node C:\Users\frank\Documents\MDDToolsHome\MDDTools\system\server\server2 remoteHost:192.168.178.25 remotePort:7575 url:/getMirons.js sourceFile:C:/Users/frank/Dropbox/DevAll/MDDToolsDataRoot/00/36.js
 node C:\Users\frank\Documents\MDDToolsHome\MDDTools\system\server\server2 remoteHost:192.168.178.25 remotePort:7575 url:/getMapList_Receptionist.js sourceFile:C:/Users/frank/Dropbox/DevAll/MDDToolsDataRoot/00/37.js
