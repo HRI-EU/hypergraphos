3386067d9bdbc70219b08bbb787e9a53c0259527
Frank Joublin <frank.joublin@honda-ri.de>
2024-10-24 09:39:50 +0000

    Update HChatWrapper.js

--- system/2.1/client/editors/HChatWrapper.js
+++ system/2.1/client/editors/HChatWrapper.js
@@ -1,11 +1,6 @@
 /*
 =============================================================================
-Licensed Materials - Property of Frank Joublin and Antonio Ceravola.
-(C) Copyright Frank Joublin and Antonio Ceravola. 2021, All Rights Reserved.
-France Government Users Restricted Rights - Use, duplication or disclosure
-restricted by GSA ADP Schedule Contract with Frank Joublin and Antonio Ceravola.
-=============================================================================
-Module: MDDTools Ace Editor Interface
+Module: HyperGraphOS Ace Editor Interface
 Date: 10.07.2020
 =============================================================================
 */
