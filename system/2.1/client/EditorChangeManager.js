963d9114cef9c033328cb2872d127ac4ca81d402
Frank Joublin <frank.joublin@honda-ri.de>
2024-10-24 09:34:49 +0000

    Update EditorChangeManager.js

--- system/2.1/client/EditorChangeManager.js
+++ system/2.1/client/EditorChangeManager.js
@@ -1,11 +1,6 @@
 /*
 =============================================================================
-Licensed Materials - Property of Frank Joublin and Antonio Ceravola.
-(C) Copyright Frank Joublin and Antonio Ceravola. 2021, All Rights Reserved.
-France Government Users Restricted Rights - Use, duplication or disclosure
-restricted by GSA ADP Schedule Contract with Frank Joublin and Antonio Ceravola.
-=============================================================================
-Module: MDDTools Manager of Change Status and Saving Process for Editors
+Module: HyperGraphOS Manager of Change Status and Saving Process for Editors
 Date: 10.07.2020
 =============================================================================
 */
