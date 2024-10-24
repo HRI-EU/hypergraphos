d5f046495433b3d859fd6e3093dc69501ea9fffd
Frank Joublin <frank.joublin@honda-ri.de>
2024-10-24 11:36:29 +0000

    Update serverManager.bat

--- system/2.1/script/serverManager.bat
+++ system/2.1/script/serverManager.bat
@@ -1,3 +1,11 @@
+REM Copyright (c) 2024 Antonello Ceravola, Frank Joublin, Honda Research Institute Europe GmbH
+REM 
+REM This file is part of HyperGraphOS.
+REM 
+REM This source code is licensed under the MIT License found in the
+REM LICENSE file in the root directory of this source tree.
+REM 
+
 @echo off
 REM Start and stop the server
 REM
