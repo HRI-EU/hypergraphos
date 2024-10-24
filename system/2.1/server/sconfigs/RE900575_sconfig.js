ae5b651f03e6eee5aa7741e88629a3ab846a4730
Frank Joublin <frank.joublin@honda-ri.de>
2024-10-24 11:55:45 +0000

    Delete RE900575_sconfig.js

--- system/2.1/server/sconfigs/RE900575_sconfig.js
+++
@@ -1,9 +0,0 @@
-const userConfigFunction = require( './Bram_sconfig.js' );
-
-const defaultUserConfigFunction = ( config )=> {
-  if( userConfigFunction ) {
-    userConfigFunction( config );
-  }
-}
-
-module.exports = defaultUserConfigFunction;
