2da4718c4a4b76497f00ab45c5668ea3079dd674
Frank Joublin <frank.joublin@honda-ri.de>
2024-10-24 11:55:56 +0000

    Delete iebubech_sconfig.js

--- system/2.1/server/sconfigs/iebubech_sconfig.js
+++
@@ -1,9 +0,0 @@
-const userConfigFunction = require( './Ebbie_sconfig.js' );
-
-const defaultUserConfigFunction = ( config )=> {
-  if( userConfigFunction ) {
-    userConfigFunction( config );
-  }
-}
-
-module.exports = defaultUserConfigFunction;
