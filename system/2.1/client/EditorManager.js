5baee6dc77267771aa7af95c30c3afdf88d13af7
Frank Joublin <frank.joublin@honda-ri.de>
2024-10-24 09:31:00 +0000

    Update EditorManager.js  remove old header

--- system/2.1/client/EditorManager.js
+++ system/2.1/client/EditorManager.js
@@ -1,10 +1,5 @@
 /*
 =============================================================================
-Licensed Materials - Property of Frank Joublin and Antonio Ceravola.
-(C) Copyright Frank Joublin and Antonio Ceravola. 2021, All Rights Reserved.
-France Government Users Restricted Rights - Use, duplication or disclosure
-restricted by GSA ADP Schedule Contract with Frank Joublin and Antonio Ceravola.
-=============================================================================
 Module: MDDTools Editor Manager
 Date: 10.07.2020
 =============================================================================
